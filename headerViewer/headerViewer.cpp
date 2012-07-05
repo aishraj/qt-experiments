a749d2a9b0c24d58f4b9b8dc1696a6e283549178
AR Dahal <dahalaishraj@gmail.com>
2012-07-05 18:48:44 +0545

    Changed the URL type

--- headerViewer/headerViewer.cpp
+++ headerViewer/headerViewer.cpp
@@ -7,8 +7,9 @@ headerViewer::headerViewer()
  QNetworkAccessManager *manager = new QNetworkAccessManager;
  connect(manager, SIGNAL(finished(QNetworkReply*)), this, SLOT(output(QNetworkReply*)));
  //manager->head(QNetworkRequest(QUrl("http://download.services.openoffice.org/files/stable/3.3.0/OOo-SDK_3.3.0_Linux_x86-64_install-deb_en-US.tar.gz")));
- manager->head(QNetworkRequest(QUrl("http://download.services.openoffice.org/files/du.list")));
+ //manager->head(QNetworkRequest(QUrl("http://download.services.openoffice.org/files/du.list")));
  //manager->head((QNetworkRequest(QUrl("http://mirror.squ.edu.om/opensuse/distribution/12.1/iso/openSUSE-12.1-DVD-i586.iso"))));
+ manager->head(QNetworkRequest(QUrl("http://ubuntu.virginmedia.com/releases//precise/ubuntu-12.04-desktop-i386.iso")));
 }
 
 headerViewer::~headerViewer()
